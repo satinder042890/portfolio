96e2d4b6efdb9fceb046f4415f0a567469a7026d
satinder <satinder042890@gmail.com>
2019-03-18 22:02:48 -0700

    index file created

--- assets/file.js
+++ assets/file.js
@@ -1,8 +1,3 @@
-function welcome(){
-    $("#mainArea").empty();
-    $("#mainArea").append("<h2 class='display-1 animated bounce'>WELCOME TO</h2>");
-    $("#mainArea").append("<h2 class='display-1 animated bounce'>MY PAGe</h2>");
-}
 
 $(document).ready(function(){
     $(".slide").on("click",function()
